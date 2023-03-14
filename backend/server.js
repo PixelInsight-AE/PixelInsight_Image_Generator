60e9f16a2d5adc321db033614b92b5dcb5811c4d
Arno luyckx <A.luyckx@protonmail.com>
2023-03-14 01:56:35 +0100

    Updated the comments slightly

--- backend/server.js
+++ backend/server.js
@@ -8,17 +8,18 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 // ! initializing the app
-
 // TODO: openai configuration
 // TODO: initialize express app
 // TODO: use cors
 // TODO: use express.json
 
 
+// ! get from front end
 // TODO: app.get hello world
 
-
+// ! post to front end
 // TODO: app.post /api/complete
 
 
+// ! listen to port
 // TODO: app.listen
