65372c4cbc86d2dc173d613f96e9f5dc37ab3885
Arno luyckx <A.luyckx@protonmail.com>
2023-03-14 01:48:05 +0100

    placed basic comments on server.js file

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+
+// ! importing all the dependencies
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+
+// ! initializing the app
+
+// TODO: openai configuration
+// TODO: initialize express app
+// TODO: use cors
+// TODO: use express.json
+
+
+// TODO: app.get hello world
+
+
+// TODO: app.post /api/complete
+
+
+// TODO: app.listen
